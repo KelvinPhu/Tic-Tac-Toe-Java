ea0b864375cc47b56d5651f8ecf4be91b426f9d0
huynh_thien_phu <phuhuynh197@gmail.com>
2024-06-12 22:28:52 +0700

    create board game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
 	public static void main(String[] args) {
+		char[][] printBoard = {
+				{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}
+		};
 		
+	}
+	
+	public static void printBoard(char[][] board) {
+		for(char[] row : board) {
+			for(char c : row) {
+				System.out.print(c);
+			}
+			System.out.println();
+		}
 	}
 }
