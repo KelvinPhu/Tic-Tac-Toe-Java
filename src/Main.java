ed59ddd8e4b3743ca11695cfcf0c18c494b58173
huynh_thien_phu <phuhuynh197@gmail.com>
2024-06-12 22:45:05 +0700

    game logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		char[][] printBoard = {
 				{' ', '|', ' ', '|', ' '},
                 {'-', '+', '-', '+', '-'},
@@ -8,6 +12,24 @@ public class Main {
                 {' ', '|', ' ', '|', ' '}
 		};
 		
+		char currentPlayer = 'X';
+		
+		while(true) {
+			printBoard(printBoard);
+			
+			System.out.println("Player " +currentPlayer+ " enter your placement [x, y]: ");
+			int row = sc.nextInt() * 2 - 2;
+			int col = sc.nextInt() * 2 - 2;
+			
+			if(row >= 0 && row < printBoard.length && col >= 0 && col < printBoard[0].length && printBoard[row][col] == ' ') {
+				printBoard[row][col] = currentPlayer;
+			}else {
+				System.out.println("Invalid placement, try again.");
+				continue;
+			}
+			
+			currentPlayer = (currentPlayer == 'X' ? 'O' : 'X');
+		}
 	}
 	
 	public static void printBoard(char[][] board) {
