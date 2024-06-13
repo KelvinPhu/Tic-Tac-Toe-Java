3248d6d6e87d5d9489abc83f13b2b0ae3ebb4449
huynh_thien_phu <phuhuynh197@gmail.com>
2024-06-13 10:36:20 +0700

    check winner

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
 		};
 		
 		char currentPlayer = 'X';
+		int filledCalls = 0;
 		
 		while(true) {
 			printBoard(printBoard);
@@ -23,6 +24,15 @@ public class Main {
 			
 			if(row >= 0 && row < printBoard.length && col >= 0 && col < printBoard[0].length && printBoard[row][col] == ' ') {
 				printBoard[row][col] = currentPlayer;
+				filledCalls++;
+				
+				if(checkWinner(printBoard, currentPlayer)) {
+					System.out.println("Player " +currentPlayer+ " win!");
+					break;
+				}else if(filledCalls == 9){
+					System.out.println("Cat!");
+					break;
+				}
 			}else {
 				System.out.println("Invalid placement, try again.");
 				continue;
@@ -39,5 +49,31 @@ public class Main {
 			}
 			System.out.println();
 		}
+	}
+	
+	public static boolean checkWinner(char[][] board, char player) {
+		// check all vertically 
+		for(int i=0; i<5; i+=2) {
+			if(board[i][0] == player && board[i][2] == player && board[i][4] == player) {
+				return true;
+			}
+		}
+		
+		// check all horizontally
+		for(int i=0; i<5; i+=2) {
+			if(board[0][i] == player && board[2][i] == player && board[4][i] == player) {
+				return true;
+			}
+		}
+		
+		// check all diagonally
+		if(board[0][0] == player && board[2][2] == player && board[4][4] == player) {
+			return true;
+		}
+		if(board[4][4] == player && board[2][2] == player && board[4][0] == player) {
+			return true;
+		}
+		
+		return false;
 	}
 }
